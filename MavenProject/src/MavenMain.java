7352fcb71c403242b062759f569d6097f2dcc1d9
SAID AL BLUSHI <said.alblushi@gmail.com>
2023-01-15 17:03:20 +0400

    \MavenProject

---
+++ MavenProject/src/MavenMain.java
@@ -0,0 +1,15 @@
+<!-- https://mvnrepository.com/artifact/org.json/json -->
+<dependency>
+    <groupId>org.json</groupId>
+    <artifactId>json</artifactId>
+    <version>20220924</version>
+</dependency>
+
+public class MavenMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
